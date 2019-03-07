c36937401b13931b562922ce8000138e07726d80
gb_90 <13396492261@163.com>
2019-03-07 15:14:57 +0800

    SpringBoot整合validate 校验  添加pom文件

---
+++ springboot-validate/src/main/java/com/gblfy/springboot/SpringbootValidateApplication.java
@@ -0,0 +1,13 @@
+package com.gblfy.springboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootValidateApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringbootValidateApplication.class, args);
+    }
+
+}
