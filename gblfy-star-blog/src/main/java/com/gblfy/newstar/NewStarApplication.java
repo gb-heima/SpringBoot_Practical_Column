20c4536a333051eb34152e9863164b133e22e32a
gb_90 <13396492261@163.com>
2019-03-08 11:10:36 +0800

    博客完成版本 SpringBoot2.0.1

---
+++ gblfy-star-blog/src/main/java/com/gblfy/newstar/NewStarApplication.java
@@ -0,0 +1,13 @@
+package com.gblfy.newstar;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NewStarApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(NewStarApplication.class, args);
+    }
+
+}
